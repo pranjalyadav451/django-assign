87f659e372ee495782da4dfe52ded2b60f3f1ddc
Pranjal Yadav <2019uec1008@mnit.ac.in>
2022-11-17 18:10:56 +0530

    some minor changes

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate auth
-python manage.py migrate
+python manage.py migrate --run-syncdb
