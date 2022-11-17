ba9612c1a674ceff3b13646683dd7fbd08eab5fc
Pranjal Yadav <2019uec1008@mnit.ac.in>
2022-11-17 20:50:44 +0530

    some minor changes

--- bitcoin_api/models.py
+++ bitcoin_api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Bitcoin(models.Model):
-    # id = models.AutoField(primary_key=True, max_length=500)
-    created_by = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        'CustomUser', max_length=500, on_delete=models.CASCADE)
     coin_name = models.CharField(max_length=500)
     current_price = models.FloatField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True, max_length=100)
@@ -12,13 +12,10 @@ class Bitcoin(models.Model):
     class Meta:
         ordering = ['-created_at']
 
-    def __str__(self):
-        return self.coin_name
-
 
 class CustomUser(AbstractUser):
     username = models.CharField(
-        max_length=500, unique=True, blank=False, null=False)
+        max_length=100, unique=True, blank=False, null=False)
     email = models.EmailField(
-        max_length=500, unique=True, blank=False, null=False)
-    password = models.CharField(max_length=500, null=False, blank=False)
+        max_length=100, unique=True, blank=False, null=False)
+    password = models.CharField(max_length=100, null=False, blank=False)
