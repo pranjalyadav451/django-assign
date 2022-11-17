3abdae12bb0c7ae1aeca8a1499c354e7929ed504
Pranjal Yadav <2019uec1008@mnit.ac.in>
2022-11-17 18:51:45 +0530

    some minor changes

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate auth
 python manage.py migrate --run-syncdb
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
